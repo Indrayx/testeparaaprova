e529364f9b30841263fa071a1c8f380f63d95c15
Lucas Augusto <lucasamgs@gmail.com>
2020-05-15 18:53:00 -0300

    leitura da escolaridade

--- src/ola.java
+++ src/ola.java
@@ -7,7 +7,33 @@ public class ola {
 		// TODO Auto-generated method stub
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
- 
+		int escolaridade,experiencia;
+		char viagem,carteira;
+		
+		System.out.println("Qual sua escolaridade?");
+		System.out.println("1) ensino fundamental");
+		System.out.println("2) ensino Medio");
+		System.out.println("3) ensino superior");
+		System.out.println("4) pós graduação");
+		System.out.print("digite uma opção:");
+		escolaridade = sc.nextInt();
+		
+		if(escolaridade == 1) {
+			escolaridade = 0;
+			escolaridade = escolaridade + 10;
+			}
+		else if(escolaridade == 2) {
+			escolaridade = 0;
+			escolaridade = escolaridade + 20;
+			}
+		else if(escolaridade == 3) {
+			escolaridade = 0;
+			escolaridade = escolaridade + 30 ;
+			}
+		else if(escolaridade == 4) {
+			escolaridade = 0;
+			escolaridade = escolaridade + 40;
+			}
 		
 		
 		
